00d801e403cec65aeec7eb545983dfcebaed002b
blue32a <blue32a.jp@gmail.com>
2024-04-26 06:39:49 +0000

    ASTを横断し、ノードへの出入りを観測する

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const esprima = require('esprima');
+const estraverse = require('estraverse');
 
 const ast = esprima.parseScript(`
 const answer = 1 + 2;
@@ -11,3 +12,15 @@ console.log('The answer is ' + answer + '.');
 `);
 
 console.dir(ast, {depth: null});
+
+let depth = 0;
+estraverse.traverse(ast, {
+  enter: function (node, parent) {
+    console.log(' '.repeat(depth) + '- enter: ' + node.type);
+    depth++;
+  },
+  leave: function (node, parent) {
+    depth--;
+    console.log(' '.repeat(depth) + '- leave: ' + node.type);
+  }
+});
